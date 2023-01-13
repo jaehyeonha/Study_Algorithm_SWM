3aa02daca5349f7bc1ffe61a9f47e9bc75fed169
mouse4432 <88102469+mouse4432@users.noreply.github.com>
2023-01-14 02:42:13 +0900

    boj are done

--- STUDY_WEEK1/12865.cpp
+++ STUDY_WEEK1/12865.cpp
@@ -16,10 +16,12 @@ int main(){
         scanf("%d %d", &weight[i], &value[i]);
     
     for(int i=1; i<=N; i++){
+        //i는 N만큼
         for(int j=1; j<=W; j++){
-            if(j>=weight[i])
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
-            else    
+            //j는 무게
+            if(j>=weight[i]) //j에 bag에 넣을 수 있는 무게일 때
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]); //앞서 넣은 bag 가치 || bag에 추가 후 가치 (앞에 있는 거 포함해서 넣을 수 있는지도 체크)
+            else//앞서 넣은 bag 가치가 가장 높음  
                 dp[i][j] = dp[i-1][j];   
         }
     }
--- STUDY_WEEK1/14501.cpp
+++ STUDY_WEEK1/14501.cpp
@@ -17,16 +17,15 @@ int main(){
 
     for(int i=0; i<=N; i++){
        
+        //i번째 즉 하루가 지나가면서 최대 이익은 max_pay인가 아님 기존의 d[i]인가
         dp[i] = max(max_pay, dp[i]);
 
-        if(t[i]+i <= N) 
+        //t[i] + i 즉 그 일을 하고도 N을 다 못 채우면, 즉 상담을 더 할 수 있다면 
+        if(t[i]+i <= N) //그 상담을 한 후에 최대 이익 = 그 일을 한 날 번째의 최대 이익 = t[i]+i
             dp[t[i]+i] = max(dp[t[i]+i], p[i] + dp[i]);
-          
         
-            
-        
+        //새로운 최대 이익이 나왔는가? 체크
         max_pay = max(max_pay, dp[i]);
-        
     }    
 
     printf("%d", max_pay);
